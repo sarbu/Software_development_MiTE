039e76d38dbd5387ea6913f755268867b3df9552
ielmeshi <ielmeshi@gmail.com>
2015-11-24 12:30:19 -0800

    Ibrahim's

---
+++ Ibramin's/Porject_303_SD_Millstone_1.0_Group_1/Main.py
@@ -0,0 +1,28 @@
+import sys
+import os
+
+
+
+
+
+def main():
+    print('                      ╔=================================================╗') 
+    print('                       well come to the Millstone 1.0 of 303_SD_Project')
+    print('                      ╚=================================================╝') 
+    print('                      ╔=================================================╗') 
+    print('                           We provide the services described below',)
+    print('                      ╚=================================================╝') 
+    
+                                       
+    print('                      ╔=================================================╗')
+    print('                       H:HomePage')
+    print('                       M:Make a reservation ') 
+    print('                       C:Canclation') 
+    print('                       G:Guests    ',)
+    print('                      ╚=================================================╝') 
+    
+
+
+
+if __name__ == '__main__': main()
+
