0647ef591f0457d7bff2935d73144b7d5a984c9c
Sarbartha S.J.B. Rana <sarbu_192@hotmail.com>
2015-11-24 12:56:31 -0800

    change1

--- Sarbu's/Porject_303_SD_Millstone_1.0_Group_1/Main.py
+++ Sarbu's/Porject_303_SD_Millstone_1.0_Group_1/Main.py
@@ -6,20 +6,20 @@ import os
 
 
 def main():
-    print('                      ╔=================================================╗') 
-    print('                       well come to the Millstone 1.0 of 303_SD_Project')
-    print('                      ╚=================================================╝') 
-    print('                      ╔=================================================╗') 
-    print('                           We provide the services described below',)
-    print('                      ╚=================================================╝') 
+    print '                      *=================================================*'
+    print '                       well come to the Millstone 1.0 of 303_SD_Project'
+    print '                      *=================================================*'
+    print '                      *=================================================*'
+    print '                           We provide the services described below'
+    print '                      *=================================================*'
     
                                        
-    print('                      ╔=================================================╗')
-    print('                       H:HomePage')
-    print('                       M:Make a reservation ') 
-    print('                       C:Canclation') 
-    print('                       G:Guests    ',)
-    print('                      ╚=================================================╝') 
+    print '                      *=================================================*'
+    print '                       H:HomePage'
+    print '                       M:Make a reservation '
+    print '                       C:Canclation'
+    print '                       G:Guests    '
+    print '                      *=================================================*'
     
 
 
